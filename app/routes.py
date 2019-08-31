4eda1c5ef44557ff5859a86c657dbecd41d4f254
Nate F <nfatkhiyev@gmail.com>
2019-08-31 06:53:58 +0100

    "Works."

--- app/routes.py
+++ app/routes.py
@@ -1,17 +1,40 @@
 from flask import render_template
 from app import app
-from gpiozero import LED
+import RPi.GPIO as GPIO
+import time
 
-led = LED()
+GPIO.setmode(GPIO.BCM)
+GPIO.setwarnings(False)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(19,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
+class Constants():
+    hasButtonBeenPressed = False
+    hasRequestBeenSent = False
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', boolean = Constants.hasButtonBeenPressed)
 
 
 @app.route('/activate')
 def activate():
-    led.on()
+    Constants.hasRequestBeenSent = True
+    GPIO.output(17,GPIO.HIGH)
+    Constants.hasButtonBeenPressed = False
     return "success"
 
+
+@app.route('/checkstatus')
+def checkstatus():
+    return 'ToDo'
+
+
+@app.route('/buttonLoop')
+def buttonLoop():
+    while Constants.hasRequestBeenSent == True:
+        if GPIO.input(19) == True:
+            Constants.hasRequestBeenSent = False
+            Constants.hasButtonBeenPressed = True
+            GPIO.output(17,GPIO.LOW)
+    return render_template('index.html', boolean = Constants.hasButtonBeenPressed)
