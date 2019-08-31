7f2e6523e9bcecc03a76291f24eb5653246ea4d1
Nate F <nfatkhiyev@gmail.com>
2019-08-31 20:01:27 +0100

    Works, long poll switches gif.

--- app/routes.py
+++ app/routes.py
@@ -2,39 +2,34 @@ from flask import render_template
 from app import app
 import RPi.GPIO as GPIO
 import time
+import sys
+import atexit
 
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
 GPIO.setup(17,GPIO.OUT)
 GPIO.setup(19,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-class Constants():
-    hasButtonBeenPressed = False
-    hasRequestBeenSent = False
+GPIO.output(17,GPIO.LOW)
 
 @app.route('/')
 def index():
-    return render_template('index.html', boolean = Constants.hasButtonBeenPressed)
-
+    return render_template('index.html')
 
 @app.route('/activate')
 def activate():
-    Constants.hasRequestBeenSent = True
+    print("Activate",file=sys.stderr)
+    startTime = time.time()
     GPIO.output(17,GPIO.HIGH)
-    Constants.hasButtonBeenPressed = False
-    return "success"
-
-
-@app.route('/checkstatus')
-def checkstatus():
-    return 'ToDo'
-
-
-@app.route('/buttonLoop')
-def buttonLoop():
-    while Constants.hasRequestBeenSent == True:
-        if GPIO.input(19) == True:
-            Constants.hasRequestBeenSent = False
-            Constants.hasButtonBeenPressed = True
+    while True:
+        elapsedTime = time.time() - startTime
+        if GPIO.input(19) == True or elapsedTime > ( 2 * 1 ):
+            print("Button pressed",file=sys.stderr)
             GPIO.output(17,GPIO.LOW)
-    return render_template('index.html', boolean = Constants.hasButtonBeenPressed)
+            return "success"
+
+def shutdown():
+    print("Goodbye",file=sys.stderr)
+    GPIO.output(17,GPIO.LOW)
+
+atexit.register(shutdown)
