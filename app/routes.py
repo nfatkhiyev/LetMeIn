e2a9551d47684228701884fb03595a0e6666c229
Nate F <nfatkhiyev@gmail.com>
2019-08-31 21:20:50 +0100

    Added second button, and (untensted) LED outputs.

--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,24 @@
 from flask import render_template
+from flask import request
 from app import app
 import RPi.GPIO as GPIO
 import time
 import sys
 import atexit
 
+PENCIL_SHARPENER = 17
+RESPONSE_BUTTON = 19
+LED_A_LEVEL = 27
+LED_1_LEVEL = 22
+
 GPIO.setmode(GPIO.BCM)
 GPIO.setwarnings(False)
-GPIO.setup(17,GPIO.OUT)
-GPIO.setup(19,GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(PENCIL_SHARPENER, GPIO.OUT)
+GPIO.setup(RESPONSE_BUTTON, GPIO.IN,  pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(LED_A_LEVEL, GPIO.OUT)
+GPIO.setup(LED_1_LEVEL, GPIO.OUT)
 
-GPIO.output(17,GPIO.LOW)
+GPIO.output(17, GPIO.LOW)
 
 @app.route('/')
 def index():
@@ -18,18 +26,34 @@ def index():
 
 @app.route('/activate')
 def activate():
-    print("Activate",file=sys.stderr)
+    level = request.args.get('level')
     startTime = time.time()
-    GPIO.output(17,GPIO.HIGH)
+
+    print("Activating: " + level, file=sys.stderr)
+
+    GPIO.output(PENCIL_SHARPENER, GPIO.HIGH)
+
+    if level == '1Level':
+        GPIO.output(LED_1_LEVEL, GPIO.HIGH)
+    elif level == 'aLevel':
+        GPIO.output(LED_A_LEVEL, GPIO.HIGH)
+
     while True:
+
         elapsedTime = time.time() - startTime
-        if GPIO.input(19) == True or elapsedTime > ( 2 * 1 ):
-            print("Button pressed",file=sys.stderr)
-            GPIO.output(17,GPIO.LOW)
+        if GPIO.input(RESPONSE_BUTTON) == True or elapsedTime > ( 2 * 1 ):
+            print("Button pressed", file=sys.stderr)
+
+            GPIO.output(PENCIL_SHARPENER, GPIO.LOW)
+            GPIO.output(LED_1_LEVEL, GPIO.LOW)
+            GPIO.output(LED_A_LEVEL, GPIO.LOW)
+
             return "success"
 
 def shutdown():
-    print("Goodbye",file=sys.stderr)
-    GPIO.output(17,GPIO.LOW)
+    print("Goodbye", file=sys.stderr)
+    GPIO.output(PENCIL_SHARPENER, GPIO.LOW)
+    GPIO.output(LED_A_LEVEL, GPIO.LOW)
+    GPIO.output(LED_1_LEVEL, GPIO.LOW)
 
 atexit.register(shutdown)
